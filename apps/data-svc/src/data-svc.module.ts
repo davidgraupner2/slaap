5fd89765e599c2f2e81a3006d761636fb22efa82
David Graupner <david.graupner@gmail.com>
2023-03-04 16:45:40 +1100

    Added Prisma

--- apps/data-svc/src/data-svc.module.ts
+++ apps/data-svc/src/data-svc.module.ts
@@ -3,9 +3,10 @@ import { DataSvcController } from './data-svc.controller';
 import { DataSvcService } from './data-svc.service';
 import { EnvConfigModule } from '@lib/common/config';
 import { CommonLoggingModule } from '@lib/common/logging';
+import { PrismaClientModule } from './prisma-client/prisma-client.module';
 
 @Module({
-  imports: [EnvConfigModule, CommonLoggingModule],
+  imports: [EnvConfigModule, CommonLoggingModule, PrismaClientModule],
   controllers: [DataSvcController],
   providers: [DataSvcService],
 })
---
+++ prisma/migrations/20230304053400_initial/migration.sql
@@ -0,0 +1,56 @@
+-- CreateTable
+CREATE TABLE "permission" (
+    "id" SERIAL NOT NULL,
+    "created_at" TIMESTAMP(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "deleted_at" TIMESTAMP(6),
+    "version" INTEGER NOT NULL,
+    "created_by" INTEGER,
+    "updated_by" INTEGER,
+    "tenant_id" INTEGER NOT NULL,
+    "entity_name" VARCHAR NOT NULL,
+    "permission" VARCHAR NOT NULL,
+
+    CONSTRAINT "PK_3b8b97af9d9d8807e41e6f48362" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "tenant" (
+    "id" SERIAL NOT NULL,
+    "created_at" TIMESTAMP(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "deleted_at" TIMESTAMP(6),
+    "version" INTEGER NOT NULL,
+    "created_by" INTEGER,
+    "updated_by" INTEGER,
+    "name" VARCHAR NOT NULL,
+    "schema_name" VARCHAR NOT NULL,
+    "description" VARCHAR,
+    "is_public" BOOLEAN NOT NULL DEFAULT false,
+    "owned_by" INTEGER,
+
+    CONSTRAINT "PK_da8c6efd67bb301e810e56ac139" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "user" (
+    "id" SERIAL NOT NULL,
+    "created_at" TIMESTAMP(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "deleted_at" TIMESTAMP(6),
+    "version" INTEGER NOT NULL,
+    "created_by" INTEGER,
+    "updated_by" INTEGER,
+    "first_name" VARCHAR NOT NULL,
+    "last_name" VARCHAR NOT NULL,
+    "user_name" VARCHAR NOT NULL,
+    "email" VARCHAR NOT NULL,
+    "password" VARCHAR NOT NULL,
+    "is_email_verified" BOOLEAN NOT NULL DEFAULT false,
+    "is_msp" BOOLEAN NOT NULL DEFAULT false,
+
+    CONSTRAINT "PK_cace4a159ff9f2512dd42373760" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "UQ_e12875dfb3b1d92d7d7c5377e22" ON "user"("email");
