1519bf00556fc5ced32ac5fb2e0d3a19c676a489
David Graupner <david.graupner@gmail.com>
2023-03-11 07:01:08 +1100

    Working on validating query params

---
+++ libs/common/src/dto/common.query.dto.ts
@@ -0,0 +1,27 @@
+import { Transform } from 'class-transformer';
+import {
+  IsBoolean,
+  IsDate,
+  IsNumber,
+  IsNumberString,
+  IsOptional,
+} from 'class-validator';
+import {
+  toBoolean,
+  toLowerCase,
+  toNumber,
+  trim,
+  toDate,
+} from '../helper/general.helpers';
+
+export class SimpleQueryDto {
+  @Transform(({ value }) => toNumber(value, { default: 0 }))
+  @IsNumber()
+  @IsOptional()
+  public page: number = 1;
+
+  @Transform(({ value }) => toBoolean(value))
+  @IsBoolean()
+  @IsOptional()
+  public showRelations = false;
+}
